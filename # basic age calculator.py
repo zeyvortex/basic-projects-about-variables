8071a3da98e61f064c4d5cb23a0e9bfaded30c06
zeyvortex <zeynep.bilen.dev@gmail.com>
2025-04-15 21:03:38 +0300

    basic age calculator

---
+++ # basic age calculator.py
@@ -0,0 +1,6 @@
+# basic age calculator
+
+birth_year = int(input("enter your birth year: "))
+current_year = int(input("please enter the current year: "))
+age = current_year - birth_year
+print(f"you are {age} years old")
