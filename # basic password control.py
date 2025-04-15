3139be18361a400e99c4cb932c78c66ba71806c7
zeyvortex <zeynep.bilen.dev@gmail.com>
2025-04-15 21:08:59 +0300

    pass control

---
+++ # basic password control.py
@@ -0,0 +1,9 @@
+# basic password control
+
+password = "Abc.1234567890"
+guess = input("please enter your password: ")
+
+if password == guess :
+    print("access granted")
+else :
+    print("access denied")
